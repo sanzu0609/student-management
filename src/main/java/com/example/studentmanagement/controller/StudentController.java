3384024fbd084d4c889a4a125f19fef9f4b20bcc
sanzu0609 <ankhang897979@gmail.com>
2025-10-05 15:50:56 +0700

    Implement StudentController with RESTful endpoints for student management

---
+++ src/main/java/com/example/studentmanagement/controller/StudentController.java
@@ -0,0 +1,9 @@
+package com.example.studentmanagement.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+}
